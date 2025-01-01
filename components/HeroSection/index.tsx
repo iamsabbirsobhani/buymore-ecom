47c2c1a71fec9464d906aeb306e258bacdce1fdb
iamsabbirsobhani <sabbirsobhani@gmail.com>
2025-01-01 22:33:02 +0600

    add(cont.) herosection

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -1,7 +1,14 @@
 export default function HeroSection() {
   return (
     <div className="hero-section">
-      <h1>Hero Section</h1>
+      <section className="bg-[#c9002e] antialiased h-[70vh] w-[70vw] flex justify-center items-center m-auto mt-[5vh] flex-col">
+        <h1 className="font-extrabold text-5xl text-gray-50">
+          Discover More. Save More. Buy More.
+        </h1>
+        <p className=" text-2xl mt-5 text-gray-50">
+          Don&apos;t miss out on exclusive offers.
+        </p>
+      </section>
     </div>
   );
 }
